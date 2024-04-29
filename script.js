7499d8c727aca6c60479cd44ee39f1caef899aae
Yamin-Thet <yaminthet886@gmail.com>
2024-04-29 20:37:54 +0630

    Finished while-fonction

---
+++ script.js
@@ -0,0 +1,25 @@
+// TODO: Write Your JavaScript Code Here
+var country = ['Korea', 'Japan', 'Thailand', 'Myanmar', 'American', 'Singapore'];
+console.log(country.length);
+for (var i=0; i<country.length; i++){
+    console.log('I want to go'+ ' ' +country[i]+'!');
+}
+
+
+// While Loop
+let n = 0;
+while(n < 11){
+    console.log(n);
+    n++
+}
+
+var letters = ["A","5","M","0","Z","2"];
+for(letter of letters){
+    console.log(letter);
+}
+
+var K = 0;
+while(K < 5){
+    console.log("This number is" + K);
+    K = K + 1
+}
